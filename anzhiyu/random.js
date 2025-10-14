37ec01c47c37d4bbac0103bfe1102794bfbb0c4d
asheep <hmxuhqq@gmail.com>
2025-10-15 02:48:10 +0800

    Site updated: 2025-10-15 02:48:08

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/14/hello-world/"];function toRandomPost(){
+var posts=["2025/10/11/ArrayList&学生管理系统/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
